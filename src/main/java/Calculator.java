808297403685130f8d4c45d9c615cf3e7251802e
morel <byox4329@gmail.com>
2021-03-12 14:42:53 +0100

    Fix checkstyle warnings

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,43 +1,48 @@
+/**
+ * Calculatrice.
+ */
 public class Calculator {
+  public int add(int valueA, int valueB) {
+    return valueA + valueB;
+  }
 
-	public int add(int valueA, int valueB) {
-		return valueA + valueB;
-	}
+  public int subtract(int valueA, int valueB) {
+    return valueA - valueB;
+  }
 
-	public int subtract(int valueA, int valueB) {
-		return valueA - valueB;
-	}
+  public int multiplie(int valueA, int valueB) {
+    return valueA * valueB;
+  }
 
-	public int multiplie(int valueA, int valueB) {
-		return valueA * valueB;
-	}
+  /**
+   * @param valueA Divided
+   * @param valueB Divider
+   * @return Division results (0 if dividing by 0)
+   */
+  public int divide(int valueA, int valueB) {
+    try {
+      return valueA / valueB;
+    } catch (Exception e) {
+      System.out.println(e);
+      return 0;
+    }
+  }
 
-	/**
-	 *
-	 * @param valueA Divided
-	 * @param valueB Divider
-	 * @return Division results (0 if dividing by 0)
-	 */
-	public int divide(int valueA, int valueB) {
-		try {
-			return valueA / valueB;
-		} catch (Exception e) {
-			System.out.println(e);
-			return 0;
-		}
-	}
+  public boolean isEven(int number) {
+    return (number % 2 == 0);
+  }
 
-	public boolean isEven(int number) {
-		return (number % 2 == 0);
-	}
-
-	public boolean isPrime(int number) {
-		for (int i = 2; i <= number / 2; i++) {
-			if (number % i == 0) {
-				return false;
-			}
-		}
-		return true;
-	}
+  /**
+   * @param number a number
+   * @return true if number is a prime number, else false
+   */
+  public boolean isPrime(int number) {
+    for (int i = 2; i <= number / 2; i++) {
+      if (number % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,67 +1,69 @@
 import java.nio.charset.Charset;
 import java.util.Scanner;
 
+
 public class Main {
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in, Charset.defaultCharset());
-		boolean exit = false;
-		while (!exit){
-			System.out.println("Choose action : ");
-			System.out.println("1: Add");
-			System.out.println("2: Sub");
-			System.out.println("3: Multiplie");
-			System.out.println("4: Divide");
-			System.out.println("5: IsEven");
-			System.out.println("6: IsPrime");
-			System.out.println("Any: Exit");
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in, Charset.defaultCharset());
+    boolean exit = false;
+    while (!exit) {
+      System.out.println("Choose action : ");
+      System.out.println("1: Add");
+      System.out.println("2: Sub");
+      System.out.println("3: Multiplie");
+      System.out.println("4: Divide");
+      System.out.println("5: IsEven");
+      System.out.println("6: IsPrime");
+      System.out.println("Any: Exit");
 
-			String choice = input.nextLine();
+      String choice = input.nextLine();
 
-			Calculator calc = new Calculator();
-			int value1, value2;
-			switch (choice){
-				case "1":
-					System.out.print("Value 1 :");
-					value1 = input.nextInt();
-					System.out.print("\nValue 2 :");
-					value2 = input.nextInt();
-					System.out.println(calc.add(value1,value2));
-					break;
-				case "2":
-					System.out.print("Value 1 :");
-					value1 = input.nextInt();
-					System.out.print("\nValue 2 :");
-					value2 = input.nextInt();
-					System.out.println(calc.subtract(value1,value2));
-					break;
-				case "3":
-					System.out.print("Value 1 :");
-					value1 = input.nextInt();
-					System.out.print("\nValue 2 :");
-					value2 = input.nextInt();
-					System.out.println(calc.multiplie(value1,value2));
-					break;
-				case "4":
-					System.out.print("Value 1 :");
-					value1 = input.nextInt();
-					System.out.print("\nValue 2 :");
-					value2 = input.nextInt();
-					System.out.println(calc.divide(value1,value2));
-					break;
-				case "5":
-					System.out.print("Value :");
-					value1 = input.nextInt();
-					System.out.println(calc.isEven(value1));
-					break;
-				case "6":
-					System.out.print("Value :");
-					value1 = input.nextInt();
-					System.out.println(calc.isPrime(value1));
-					break;
-				default:
-					exit = true;
-			}
-		}
-		input.close();
-	}
+      Calculator calc = new Calculator();
+      int value1;
+      int value2;
+      switch (choice) {
+        case "1":
+          System.out.print("Value 1 :");
+          value1 = input.nextInt();
+          System.out.print("\nValue 2 :");
+          value2 = input.nextInt();
+          System.out.println(calc.add(value1, value2));
+          break;
+        case "2":
+          System.out.print("Value 1 :");
+          value1 = input.nextInt();
+          System.out.print("\nValue 2 :");
+          value2 = input.nextInt();
+          System.out.println(calc.subtract(value1, value2));
+          break;
+        case "3":
+          System.out.print("Value 1 :");
+          value1 = input.nextInt();
+          System.out.print("\nValue 2 :");
+          value2 = input.nextInt();
+          System.out.println(calc.multiplie(value1, value2));
+          break;
+        case "4":
+          System.out.print("Value 1 :");
+          value1 = input.nextInt();
+          System.out.print("\nValue 2 :");
+          value2 = input.nextInt();
+          System.out.println(calc.divide(value1, value2));
+          break;
+        case "5":
+          System.out.print("Value :");
+          value1 = input.nextInt();
+          System.out.println(calc.isEven(value1));
+          break;
+        case "6":
+          System.out.print("Value :");
+          value1 = input.nextInt();
+          System.out.println(calc.isPrime(value1));
+          break;
+        default:
+          exit = true;
+      }
+    }
+    input.close();
+  }
 }
