2cc486037816f76dc21fd5d13c56a2b6b3d9e483
morel <byox4329@gmail.com>
2021-03-12 09:11:46 +0100

    Calc Test + Jacoco Reports

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,5 +1,39 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testAdd(){
+        int a = 2;
+        int b = 3;
+
+        Calculator calc = new Calculator();
+        int actual = calc.add(a,b);
+
+        assertEquals(5, actual);
+    }
+    @Test
+    public void testSub(){
+        int a = 2;
+        int b = 3;
+
+        Calculator calc = new Calculator();
+        int actual = calc.subtract(a,b);
+
+        assertEquals(-1, actual);
+    }
 
 }
