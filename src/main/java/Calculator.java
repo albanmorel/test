fc3bec56cccd058bcc20abe778fe54d0fd15bbd1
morel <byox4329@gmail.com>
2021-03-12 10:40:46 +0100

    code refactor

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,9 +1,10 @@
 public class Calculator {
 
-    public int add(int valueA, int valueB) {
-        return valueA+valueB;
-    }
-    public int subtract(int valueA, int valueB) {
-        return valueA-valueB;
-    }
+	public int add(int valueA, int valueB) {
+		return valueA + valueB;
+	}
+
+	public int subtract(int valueA, int valueB) {
+		return valueA - valueB;
+	}
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,34 +6,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
-    @Before
-    public void setUp() throws Exception {
+	@Before
+	public void setUp() throws Exception {
 
-    }
-    @After
-    public void tearDown() throws Exception {
+	}
 
-    }
+	@After
+	public void tearDown() throws Exception {
 
-    @org.junit.jupiter.api.Test
-    public void testAdd(){
-        int valueA = 2;
-        int valueB = 3;
+	}
 
-        Calculator calc = new Calculator();
-        int actual = calc.add(valueA,valueB);
+	@org.junit.jupiter.api.Test
+	public void testAdd() {
+		int valueA = 2;
+		int valueB = 3;
 
-        assertEquals(5, actual);
-    }
-    @org.junit.jupiter.api.Test
-    public void testSub(){
-        int valueA = 2;
-        int valueB = 3;
+		Calculator calc = new Calculator();
+		int actual = calc.add(valueA, valueB);
 
-        Calculator calc = new Calculator();
-        int actual = calc.subtract(valueA,valueB);
+		assertEquals(5, actual);
+	}
 
-        assertEquals(-1, actual);
-    }
+	@org.junit.jupiter.api.Test
+	public void testSub() {
+		int valueA = 2;
+		int valueB = 3;
+
+		Calculator calc = new Calculator();
+		int actual = calc.subtract(valueA, valueB);
+
+		assertEquals(-1, actual);
+	}
 
 }
